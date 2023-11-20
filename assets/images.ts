95f926f7b21668ec5d9e55469417e6a6acb75242
Akshat Mittal <akshatmittal2506@gmail.com>
2023-11-20 19:00:39 +0530

    feat: added data to notifications component and static assets

---
+++ assets/images.ts
@@ -0,0 +1,91 @@
+const collection = "/images/collection.png";
+const creatorbackground1 = "/images/creatorbackground-1.jpeg";
+const creatorbackground2 = "/images/creatorbackground-2.jpeg";
+const creatorbackground3 = "/images/creatorbackground-3.jpeg";
+const creatorbackground4 = "/images/creatorbackground-4.jpg";
+const creatorbackground5 = "/images/creatorbackground-5.jpg";
+const creatorbackground6 = "/images/creatorbackground-6.jpg";
+const creatorbackground7 = "/images/creatorbackground-7.jpg";
+const creatorbackground8 = "/images/creatorbackground-8.jpg";
+const creatorbackground9 = "/images/creatorbackground-9.jpg";
+const creatorbackground10 = "/images/creatorbackground-10.jpg";
+const creatorbackground11 = "/images/creatorbackground-11.jpg";
+const user1 = "/images/user-1.png";
+const user2 = "/images/user-2.png";
+const user3 = "/images/user-3.png";
+const user4 = "/images/user-4.png";
+const user5 = "/images/user-5.png";
+const user6 = "/images/user-6.png";
+const user7 = "/images/user-7.png";
+const user8 = "/images/user-8.png";
+const user9 = "/images/user-9.png";
+const user10 = "/images/user-10.png";
+const service1 = "/images/service-1.png";
+const service2 = "/images/service-2.png";
+const service3 = "/images/service-3.png";
+const service4 = "/images/service-4.png";
+const logoLight = "/images/logo-light.svg";
+const logo = "/images/logo.svg";
+const facebook = "/images/facebook.svg";
+const twitter = "/images/twitter.svg";
+const telegram = "/images/telegram.svg";
+const youtube = "/images/youtube.svg";
+const vimeo = "/images/vimeo.svg";
+const vector = "/images/VectorHIW.svg";
+const zgcmq = "/images/zgcmq.svg";
+const hero = "/images/hero.png";
+const nft_1 = "/images/nft_1.png";
+const getAble = "/images/getable.png";
+const nft_image_1 = "/images/nft-image-1.png";
+const nft_image_2 = "/images/nft-image-2.png";
+const nft_image_3 = "/images/nft-image-3.png";
+const musiceWave = "/images/musicwave.png";
+const update = "/images/update.png";
+
+const images = {
+	collection,
+	creatorbackground1,
+	creatorbackground2,
+	creatorbackground3,
+	creatorbackground4,
+	creatorbackground5,
+	creatorbackground6,
+	creatorbackground7,
+	creatorbackground8,
+	creatorbackground9,
+	creatorbackground10,
+	creatorbackground11,
+	user1,
+	user2,
+	user3,
+	user4,
+	user5,
+	user6,
+	user7,
+	user8,
+	user9,
+	user10,
+	service1,
+	service2,
+	service3,
+	service4,
+	logoLight,
+	logo,
+	facebook,
+	twitter,
+	telegram,
+	youtube,
+	vimeo,
+	vector,
+	zgcmq,
+	hero,
+	nft_1,
+	getAble,
+	nft_image_1,
+	nft_image_2,
+	nft_image_3,
+	musiceWave,
+	update,
+};
+
+export default images;
--- components/Header/Notifications/index.tsx
+++ components/Header/Notifications/index.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 import { stylesConfig } from "@/utils/functions";
 import styles from "./styles.module.scss";
+import { Typography } from "@/library";
+import Image from "next/image";
+import images from "@/assets/images";
 
 const classes = stylesConfig(styles, "header-notifications");
 
 const HeaderNotifications: React.FC = () => {
-	return <div className={classes("")}></div>;
+	return (
+		<div className={classes("")}>
+			<Typography>Notifications</Typography>
+			<div className={classes("-box")}>
+				<div className={classes("-box-img")}>
+					<Image
+						src={images.user1}
+						alt="user1"
+						width={50}
+						height={50}
+					/>
+				</div>
+				<div className={classes("-box-content")}>
+					<Typography as="h4">Akshat Mittal</Typography>
+					<Typography as="address">Gibberish</Typography>
+					<Typography as="small">7 minutes ago</Typography>
+				</div>
+				<div className={classes("-box-new")}></div>
+			</div>
+		</div>
+	);
 };
 
 export default HeaderNotifications;
