76daf4ba6cea2c494d92a2db78e06fb97993e1dc
Akshat Mittal <akshatmittal2506@gmail.com>
2023-11-20 18:47:36 +0530

    feat: added data to discover component

--- components/Header/Discover/index.tsx
+++ components/Header/Discover/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { stylesConfig } from "@/utils/functions";
 import styles from "./styles.module.scss";
+import { discoverMenu } from "@/constants/header";
+import Link from "next/link";
 
 const classes = stylesConfig(styles, "header-discover");
 
 const HeaderDiscover: React.FC = () => {
-	return <div className={classes("")}></div>;
+	return (
+		<div className={classes("")}>
+			{discoverMenu.map((item, index) => (
+				<div
+					key={`header-discover-menu-${index}`}
+					className={classes("item")}
+				>
+					<Link href={item.path}>{item.name}</Link>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default HeaderDiscover;
---
+++ constants/header.ts
@@ -0,0 +1,33 @@
+export const discoverMenu: {
+	name: string;
+	path: string;
+}[] = [
+	{
+		name: "Collection",
+		path: "collection",
+	},
+	{
+		name: "Search",
+		path: "search",
+	},
+	{
+		name: "Author Profile",
+		path: "author-profile",
+	},
+	{
+		name: "NFT Details",
+		path: "NFT-details",
+	},
+	{
+		name: "Account Setting",
+		path: "account-setting",
+	},
+	{
+		name: "Connect Wallet",
+		path: "connect-wallet",
+	},
+	{
+		name: "Blog",
+		path: "blog",
+	},
+];
