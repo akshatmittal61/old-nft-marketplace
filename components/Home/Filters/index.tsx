b9a4f082b604f2a82747957d0a8905cb922e02b7
Akshat Mittal <akshatmittal2506@gmail.com>
2023-11-28 16:31:11 +0530

    feat: filters section

--- components/Home/Filters/index.tsx
+++ components/Home/Filters/index.tsx
@@ -11,7 +11,6 @@ import {
 } from "react-icons/fa";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { MdVerified } from "react-icons/md";
-import { TiTick } from "react-icons/ti";
 import { Button } from "@/library";
 import styles from "./styles.module.scss";
 import { stylesConfig } from "@/utils/functions";
@@ -24,79 +23,6 @@ const Filter = () => {
 	const [video, setVideo] = useState(true);
 	const [music, setMusic] = useState(true);
 
-	/* return (
-		<div className={Style.filter}>
-			<div className={Style.filter_box}>
-				<div className={Style.filter_box_left}>
-					<Button>NFTs</Button>
-					<Button>Arts</Button>
-					<Button>Musics</Button>
-					<Button>Sports</Button>
-					<Button>Photography</Button>
-				</div>
-
-				<div className={Style.filter_box_right}>
-					<div
-						className={Style.filter_box_right_box}
-						onClick={() => setFilter((p) => !p)}
-					>
-						<FaFilter />
-						<span>Filter</span>{" "}
-						{filter ? <FaAngleDown /> : <FaAngleUp />}
-					</div>
-				</div>
-			</div>
-
-			{filter && (
-				<div className={Style.filter_box_items}>
-					<div className={Style.filter_box_items_box}>
-						<div className={Style.filter_box_items_box_item}>
-							<FaWallet /> <span>10 ETH</span>
-							<AiFillCloseCircle />
-						</div>
-					</div>
-
-					<div className={Style.filter_box_items_box}>
-						<div
-							className={Style.filter_box_items_box_item_trans}
-							onClick={() => setImage((p) => !p)}
-						>
-							<FaImages /> <small>Images</small>
-							{image ? <AiFillCloseCircle /> : <TiTick />}
-						</div>
-					</div>
-
-					<div className={Style.filter_box_items_box}>
-						<div
-							className={Style.filter_box_items_box_item_trans}
-							onClick={() => setVideo((p) => !p)}
-						>
-							<FaVideo /> <small>Videos</small>
-							{video ? <AiFillCloseCircle /> : <TiTick />}
-						</div>
-					</div>
-
-					<div className={Style.filter_box_items_box}>
-						<div
-							className={Style.filter_box_items_box_item_trans}
-							onClick={() => setMusic((p) => !p)}
-						>
-							<FaMusic /> <small>Musics</small>
-							{music ? <AiFillCloseCircle /> : <TiTick />}
-						</div>
-					</div>
-
-					<div className={Style.filter_box_items_box}>
-						<div className={Style.filter_box_items_box_item}>
-							<FaUserAlt /> <span>Verified</span>
-							<MdVerified />
-						</div>
-					</div>
-				</div>
-			)}
-		</div>
-	); */
-
 	return (
 		<div className={classes("")}>
 			<div className={classes("-box")}>
@@ -107,40 +33,101 @@ const Filter = () => {
 					<Button>Sports</Button>
 					<Button>Photography</Button>
 				</div>
-
 				<div className={classes("-box-right")}>
-					<div
+					<Button
+						variant="filled"
 						className={classes("-box-right-box")}
 						onClick={() => setShowFilter((p) => !p)}
 					>
 						<FaFilter />
 						<span>Filter</span>{" "}
 						{showFilter ? <FaAngleDown /> : <FaAngleUp />}
-					</div>
+					</Button>
 				</div>
 			</div>
 			{showFilter ? (
 				<div className={classes("-options")}>
-					<div className={classes("-option")}>
+					{/* <div className={classes("-option", "-option--active")}>
 						<FaWallet /> <span>10 ETH</span>
-						<AiFillCloseCircle />
 					</div>
-					<div className={classes("-option")}>
+					<div
+						className={classes("-option", {
+							"-option--active": image,
+						})}
+						onClick={() => setImage((p) => !p)}
+					>
 						<FaImages /> <small>Images</small>
-						{image ? <AiFillCloseCircle /> : <TiTick />}
+						{image ? <AiFillCloseCircle /> : null}
 					</div>
-					<div className={classes("-option")}>
+					<div
+						className={classes("-option", {
+							"-option--active": video,
+						})}
+						onClick={() => setVideo((p) => !p)}
+					>
 						<FaVideo /> <small>Videos</small>
-						{video ? <AiFillCloseCircle /> : <TiTick />}
+						{video ? <AiFillCloseCircle /> : null}
 					</div>
-					<div className={classes("-option")}>
-						<FaMusic /> <small>Musics</small>
-						{music ? <AiFillCloseCircle /> : <TiTick />}
+					<div
+						className={classes("-option", {
+							"-option--active": music,
+						})}
+						onClick={() => setMusic((p) => !p)}
+					>
+						<FaMusic /> <small>Music</small>
+						{music ? <AiFillCloseCircle /> : null}
 					</div>
 					<div className={classes("-option")}>
 						<FaUserAlt /> <span>Verified</span>
 						<MdVerified />
-					</div>
+					</div> */}
+					<Button
+						icon={<FaWallet />}
+						iconPosition="left"
+						size="small"
+						variant="filled"
+					>
+						10 ETH
+					</Button>
+					<Button
+						icon={<FaImages />}
+						iconPosition="left"
+						size="small"
+						variant={image ? "filled" : "outlined"}
+						onClick={() => setImage((p) => !p)}
+					>
+						Images
+						{image ? <AiFillCloseCircle /> : null}
+					</Button>
+					<Button
+						icon={<FaVideo />}
+						iconPosition="left"
+						size="small"
+						variant={video ? "filled" : "outlined"}
+						onClick={() => setVideo((p) => !p)}
+					>
+						Videos
+						{video ? <AiFillCloseCircle /> : null}
+					</Button>
+					<Button
+						icon={<FaMusic />}
+						iconPosition="left"
+						size="small"
+						variant={music ? "filled" : "outlined"}
+						onClick={() => setMusic((p) => !p)}
+					>
+						Music
+						{music ? <AiFillCloseCircle /> : null}
+					</Button>
+					<Button
+						icon={<FaUserAlt />}
+						iconPosition="left"
+						size="small"
+						variant="outlined"
+					>
+						Verified
+						<MdVerified />
+					</Button>
 				</div>
 			) : null}
 		</div>
